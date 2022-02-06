34d348d774c1f08dec4b10c4688a43b78c7ee00a
Amirhossein <amirhosseinsaloot@gmail.com>
2022-02-06 19:23:46 +0330

    Remove unused Dto RefreshToken.cs

--- src/Presentation/Api/Dtos/AuthToken/RefreshToken.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Api.Dtos.AuthToken;
-
-public class RefreshToken
-{
-    public string refresh_token { get; set; } = null!;
-
-    public DateTime refresh_token_expires_in { get; set; }
-}
