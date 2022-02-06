84ecd5a411ae00e3123ca01dbaa33cfb94a764d1
Amirhossein <amirhosseinsaloot@gmail.com>
2022-02-06 18:26:54 +0330

    remove accessTokenExpiresIn from frontend

--- src/Presentation/dotnetcore_vuejs/src/models/authentication/token.js
+++ src/Presentation/dotnetcore_vuejs/src/models/authentication/token.js
@@ -1,12 +1,10 @@
 function Token({
   accessToken,
-  accessTokenExpiresIn,
   refreshToken,
   refreshTokenExpiresIn,
   tokenType
 }) {
   this.accessToken = accessToken;
-  this.accessTokenExpiresIn = accessTokenExpiresIn;
   this.refreshToken = refreshToken;
   this.refreshTokenExpiresIn = refreshTokenExpiresIn;
   this.tokenType = tokenType;
